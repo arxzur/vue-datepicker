056cde0db22aaaaae1acf57b4e9fed852749f4f6
arxzur <arxzur@proton.me>
2022-10-26 12:08:22 +0200

    Fix dependency issue

--- src/components/__tests__/CalendarMonth.spec.ts
+++ src/components/__tests__/CalendarMonth.spec.ts
@@ -4,18 +4,6 @@ import { mount } from '@vue/test-utils'
 import CalendarMonth from '../CalendarMonth.vue'
 
 describe('CalendarMonth', () => {
-  it('renders event button text properly', () => {
-    const wrapper = mount(CalendarMonth, {
-      global: {
-        stubs: {
-          FontAwesomeIcon: true
-        }
-      },
-      props: { eventButtonName: 'Hello Vitest' }
-    })
-
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
   it('renders all calendar buttons', () => {
     const wrapper = mount(CalendarMonth, {
       global: {
@@ -27,9 +15,22 @@ describe('CalendarMonth', () => {
 
     expect(wrapper.findAll('button')).toHaveLength(42)
   })
+  it('renders event button text', () => {
+    const wrapper = mount(CalendarMonth, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true
+        }
+      },
+      props: { eventButtonName: 'Hello Vitest' }
+    })
 
+    expect(wrapper.text()).toContain('Hello Vitest')
+  })
+
+  // snapshot
+  // before each
   // props
   // events emmitting
-  // snapshot
 
 })
